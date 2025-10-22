044d7a779ca0dc9404c12b6c4d780fefd14b8baa
waadhmd <waad.hamoud94@gmail.com>
2025-10-22 15:21:10 +0200

    initialize database

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.db.database import init_db, engine
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("Starting PawBase API...")
+    init_db()
+    yield
+    print("Shutting down PawBase API...")
+    engine.dispose()
+
 
 app = FastAPI(title="PawBase API",
               version="1.0.0",
-              description="API for the PawBase animal shelter")
-
+              description="API for the PawBase animal shelter",
+              lifespan=lifespan)
 
 @app.get("/")
 def read_root():
