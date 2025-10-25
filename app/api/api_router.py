65d902f7f3ee57d2f171d0ee08f503f127975978
waadhmd <waad.hamoud94@gmail.com>
2025-10-25 21:01:45 +0200

    add 'internal/auth' route

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.routers.internal import users
+from app.api.routers.internal import users, auth as internal_auth
 #from app.api.v1.routers.public import animals as public_animals
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/internal/users", tags=["Internal - Users"])
+api_router.include_router(internal_auth.router, prefix="/internal/auth", tags=["Internal - Auth"])
 #api_v1_router.include_router(public_animals.router, prefix="/public/animals", tags=["Public - Animals"])
